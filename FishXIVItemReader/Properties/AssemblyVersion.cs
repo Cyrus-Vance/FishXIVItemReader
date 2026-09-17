32700617527b64c30fc6fe296b2107a9910c6b54
Cyrus Vance <493093054@qq.com>
2026-09-17 16:56:02 +0800

    版本更新至 v0.0.0.18

--- FishXIVItemReader/Properties/AssemblyVersion.cs
+++ FishXIVItemReader/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.0.17")]
-[assembly: AssemblyFileVersion("0.0.0.17")]
+[assembly: AssemblyVersion("0.0.0.18")]
+[assembly: AssemblyFileVersion("0.0.0.18")]
